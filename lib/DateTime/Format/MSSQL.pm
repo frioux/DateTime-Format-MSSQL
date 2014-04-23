3a539ab0468aa963ca427bc00ba619c194889395
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-04-23 18:08:10 -0500

    Allow passing time_zone during instantiation

--- lib/DateTime/Format/MSSQL.pm
+++ lib/DateTime/Format/MSSQL.pm
@@ -5,13 +5,17 @@ use strict;
 use warnings;
 use DateTime::Format::Strptime;
 
-my $format = DateTime::Format::Strptime->new(
-  pattern => '%Y-%m-%d %H:%M:%S.%3N'
-);
+sub new {
+   shift;
+   DateTime::Format::Strptime->new(
+     @_,
+     pattern => '%Y-%m-%d %H:%M:%S.%3N'
+   )
+}
 
-sub parse_datetime   { shift; $format->parse_datetime(@_) }
+sub parse_datetime   { shift->new->parse_datetime(@_) }
 
-sub format_datetime  { shift; $format->format_datetime(@_) }
+sub format_datetime  { shift->new->format_datetime(@_) }
 
 1;
 
@@ -34,6 +38,15 @@ __END__
 This is just a basic module to help parse dates formatted from SQL Server.
 
 =head1 METHODS
+
+=head2 new
+
+Instantiate a new C<DateTime::Format::MSSQL>.  You can override the
+C<time_zone> that the parsed date is returned as by passing it as an argument:
+
+ DateTime::Format::MSSQL->new(
+    time_zone => 'local',
+ )->parse_datetime($str)
 
 =head2 parse_datetime
 
